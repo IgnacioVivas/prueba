07ac8676f3c2f4fd2f577b0a4e8df5f067acc547
IgnacioVivas <ignacab@hotmail.com>
2022-11-02 12:25:29 -0300

    actualizando import de config

--- src/database/database.js
+++ src/database/database.js
@@ -1,5 +1,5 @@
 import Sequelize from 'sequelize';
-import { DB_HOST, DB_NAME, DB_PASSWORD, DB_PORT, DB_USER } from '../config';
+import { DB_HOST, DB_NAME, DB_PASSWORD, DB_PORT, DB_USER } from '../config.js';
 
 // export const sequelize = new Sequelize('prueba', 'postgres', 'homeroaleli', {
 //   host: 'localhost',
