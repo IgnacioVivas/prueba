3749c38283be5e16559782acd605e04baf484bd1
IgnacioVivas <ignacab@hotmail.com>
2022-11-02 12:38:15 -0300

    actualizando archivo config

--- src/database/database.js
+++ src/database/database.js
@@ -6,6 +6,11 @@ import { DB_HOST, DB_NAME, DB_PASSWORD, DB_PORT, DB_USER } from '../config.js';
 //   dialect: 'postgres',
 // });
 
+// database: DB_NAME,
+//   username: DB_USER,
+//   password: DB_PASSWORD,
+//   host: DB_HOST,
+//   port: DB_PORT,
 export const sequelize = new Sequelize({
   database: DB_NAME,
   username: DB_USER,
