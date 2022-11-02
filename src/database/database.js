33604774d7639042bde535ac17c7bad74aa1de4e
IgnacioVivas <ignacab@hotmail.com>
2022-11-02 12:17:50 -0300

    agregando el comando start

--- src/database/database.js
+++ src/database/database.js
@@ -1,6 +1,21 @@
 import Sequelize from 'sequelize';
 
-export const sequelize = new Sequelize('prueba', 'postgres', 'homeroaleli', {
-  host: 'localhost',
+// export const sequelize = new Sequelize('prueba', 'postgres', 'homeroaleli', {
+//   host: 'localhost',
+//   dialect: 'postgres',
+// });
+
+export const sequelize = new Sequelize({
+  database: 'railway',
+  username: 'postgres',
+  password: 'aH4A5k105L3Npd3P3MgK',
+  host: 'containers-us-west-104.railway.app',
+  port: 6434,
   dialect: 'postgres',
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false, // <<<<<<< YOU NEED THIS
+    },
+  },
 });
